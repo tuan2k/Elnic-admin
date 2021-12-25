105d2c241c20fecc4a7248b4374497f14920373c
Nguyen Thanh Tuan <anhmaygalam15.tn@gmail.com>
2021-12-25 23:03:31 +0700

    optimize code

--- src/components/order/index.vue
+++ src/components/order/index.vue
@@ -17,95 +17,6 @@
               <div class="card-body custom-tab-1">
                 <div class="tab-content">
                   <div id="my-posts" class="tab-pane fade active show">
-<<<<<<< HEAD
-                    <div
-                      class="col-xl-12 col-xxl-12 col-lg-12 col-md-12 col-sm-12"
-                    >
-                      <div>
-                        <h5 class="text-primary mb-4">Danh sách đơn hàng</h5>
-                        <div class="card-body">
-                          <div class="table-responsive">
-                            <table
-                              class="table header-border table-hover verticle-middle  table-bordered"
-                            >
-                              <thead>
-                                <tr>
-                                  <th><strong>STT</strong></th>
-                                  <th><strong>Tên người mua</strong></th>
-                                  <th><strong>Email</strong></th>
-                                  <th><strong>Số điện thoại</strong></th>
-                                  <th><strong>Tổng tiền</strong></th>
-                                  <th><strong>Chức năng</strong></th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr v-for="(td, index) in orders" :key="td._id">
-                                  <td>{{ index + 1 }}</td>
-                                  <td>
-                                    <span v-for="u in users" :key="u._id">
-                                      <span v-if="td.userId === u._id">
-                                        {{ u.username }}</span
-                                      >
-                                    </span>
-                                  </td>
-                                  <td>{{ td.email }}</td>
-                                  <td>{{ td.phone }}</td>
-                                  <td>10</td>
-                                  <td>
-                                    <router-link
-                                      :to="{
-                                        name: 'orderDetail',
-                                        params: { id: td._id }
-                                      }"
-                                      class="btn btn-primary"
-                                    >
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="16"
-                                        height="16"
-                                        fill="currentColor"
-                                        class="bi bi-pip"
-                                        viewBox="0 0 16 16"
-                                      >
-                                        <path
-                                          d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"
-                                        />
-                                        <path
-                                          d="M8 8.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5v-3z"
-                                        />
-                                      </svg>
-                                      Xem
-                                    </router-link>
-                                    <!-- <span
-                                      class="btn btn-danger"
-                                      v-on:click="deleteOrder(td._id)"
-                                    >
-                                      <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="16"
-                                        height="16"
-                                        fill="currentColor"
-                                        class="bi bi-trash"
-                                        viewBox="0 0 16 16"
-                                      >
-                                        <path
-                                          d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-                                        />
-                                        <path
-                                          fill-rule="evenodd"
-                                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                                        />
-                                      </svg>
-                                      Xóa
-                                    </span> -->
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          </div>
-                        </div>
-                      </div>
-=======
                     <div class="col-xl-12 col-xxl-12 col-lg-12 col-md-12 col-sm-12">
                                                     <div>
                                                         <h5 class="text-primary mb-4">Danh sách đơn hàng</h5>
@@ -149,7 +60,6 @@
                                                             </div>
                                                         </div>
                                                     </div>
->>>>>>> cc6aa63d05dd7d1aa033bbfb1b4c595123d5c539
                     </div>
                   </div>
                 </div>
